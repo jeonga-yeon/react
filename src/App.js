d7f7971c47b630bbe9e7d9db55a0f392f4266a84
joyfuldev <wings2132@naver.com>
2022-10-18 20:59:20 +0900

    game result

--- src/App.js
+++ src/App.js
@@ -1,17 +1,61 @@
+import { useState } from "react";
 import "./App.css";
 import Box from "./components/Box";
 
+const choice = {
+  rock: {
+    name: "Rock",
+    img: "https://images.unsplash.com/photo-1589694502732-74708bde4e3e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzZ8fHJvY2t8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+  },
+  scissors: {
+    name: "Scissor",
+    img: "https://images.unsplash.com/photo-1584065422336-7844ec884107?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  },
+  paper: {
+    name: "Paper",
+    img: "https://images.unsplash.com/photo-1595113316349-9fa4eb24f884?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80",
+  },
+};
+
 function App() {
+  const [userSelect, setUserSelect] = useState(null);
+  const [computerSelect, setComputerSelect] = useState(null);
+  const [result, setResult] = useState("");
+  const play = (userChoice) => {
+    setUserSelect(choice[userChoice]);
+    let computerChoice = randomChoice();
+    setComputerSelect(computerChoice);
+    setResult(judgement(choice[userChoice], computerChoice));
+  };
+
+  const judgement = (user, computer) => {
+    if (user.name === computer.name) {
+      return "tie";
+    } else if (user.name === "Rock")
+      return computer.name === "Scissor" ? "win" : "lose";
+    else if (user.name === "Scissor")
+      return computer.name === "Paper" ? "win" : "lose";
+    else if (user.name === "Paper")
+      return computer.name === "Rock" ? "win" : "lose";
+  };
+
+  const randomChoice = () => {
+    let itemArray = Object.keys(choice);
+    let randomItem = Math.floor(Math.random() * itemArray.length);
+    let final = itemArray[randomItem];
+    return choice[final];
+  };
+
   return (
     <div>
       <div className="main">
-        <Box title="You" />
-        <Box title="Computer" />
+        <Box title="You" item={userSelect} result={result} />
+        <Box title="Computer" item={computerSelect} result={result} />
       </div>
       <div className="main">
-        <button>가위</button>
-        <button>바위</button>
-        <button>보</button>
+        <button onClick={() => play("scissors")}>가위</button>
+        <button onClick={() => play("rock")}>바위</button>
+        <button onClick={() => play("paper")}>보</button>
       </div>
     </div>
   );
--- src/components/Box.js
+++ src/components/Box.js
@@ -1,15 +1,12 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React from "react";
 
-const Box = ({ title }) => {
+const Box = ({ title, item, result }) => {
   return (
     <div className="box">
       <h1>{title}</h1>
-      <img
-        className="item-img"
-        src="https://images.unsplash.com/photo-1589694502732-74708bde4e3e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-      />
-      <h2>WIN</h2>
+      <img className="item-img" src={item && item.img} />
+      <h2>{result}</h2>
     </div>
   );
 };
